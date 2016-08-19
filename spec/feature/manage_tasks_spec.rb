833dfbd25d544dcccc45f1d457240da203c77fd3
Ecthelion3 <matthijs.vandenberg@gmail.com>
2016-08-19 11:11:51 +0200

    add test 3

--- spec/feature/manage_tasks_spec.rb
+++ spec/feature/manage_tasks_spec.rb
@@ -2,16 +2,10 @@ require 'rails_helper'
 
 feature 'Manage tasks', js: true do
   scenario 'add a new task' do
-    # Point your browser towards the todo path
     visit todos_path
-
-    # Enter description in the text field
     fill_in 'todo_title', with: 'Be Batman'
-
-    # Press enter to submit the form
     page.execute_script("$('form').submit()")
 
-    # Expect the new task to be displayed in the list of tasks
     expect(page).to have_content('Be Batman')
   end
 
@@ -20,9 +14,20 @@ feature 'Manage tasks', js: true do
     fill_in 'todo_title', with: 'Eat a cheese burger'
     page.execute_script("$('form').submit()")
 
-    # Wait for 1 second so the counter can be updated
     sleep(1)
 
     expect(page.find(:css, 'span#todo-count').text).to eq "1"
+  end
+
+  scenario 'complete a task' do
+    visit todos_path
+    fill_in 'todo_title', with: 'go to candy mountain'
+    page.execute_script("$('form').submit()")
+
+    check('todo-1')
+
+    sleep(1)
+
+    expect(page.find(:css, 'span#todo-count').text).to eq "0"
   end
 end
